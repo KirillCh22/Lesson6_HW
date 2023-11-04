9ff2cfc08a225588b2987f2a3ca05558886f1ee2
Kirill <147234461+KirillCh22@users.noreply.github.com>
2023-11-04 18:42:50 +0300

    Completed HomeWork.

---
+++ Task41/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 41
+//  Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел больше 0 ввел пользователь.
+// * 0, 7, 8, -2  ->  2
+// * -1, -7, 567, 89, 223  ->  3
+
+
+int[] GenerateArray()
+{
+    string[] str;
+    Console.WriteLine("Введите числа через , :");
+    str = Console.ReadLine().Split(",");
+
+    int[] numbers = new int[str.Length];
+    for(int i = 0; i < str.Length; i++)
+    {
+        numbers[i] = Convert.ToInt32(str[i]);
+    }
+
+    return numbers;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine();
+    Console.Write("Массив: [");
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1) Console.Write(array[i] + "]");
+        else Console.Write(array[i] + ", ");
+    }
+}
+
+
+void CountNegativeNumbers(int[] array)
+{
+    Console.WriteLine();
+    int negative_numbers = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0 ) negative_numbers++; 
+    }
+
+    Console.Write($"Количество чисел больше 0, которые ввел пользователь - {negative_numbers}.");
+}
+
+
+void Main()
+{
+    int[] array = GenerateArray();
+    PrintArray(array);
+    Console.WriteLine();
+    CountNegativeNumbers(array);
+}
+
+
+Main();
---
+++ Task43/Program.cs
@@ -0,0 +1,57 @@
+﻿//  Задача 43
+//  Напишите программу, которая найдет точку пересечения двух прямых, 
+//  заданных уравнениями y = k1 * x + b1 , y = k2 * x + b2 ; значения b1, k1, b2, k2 задаются пользователем.
+
+//  b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5: -0,5)
+
+//  Приравниваем правые части данных равенств: 
+//  k1 * x + b1 = k2 * x + b2 
+//  выразим отсюда x: 
+//  x = (b2 - b1) / (k1 - k2) 
+//  Затем подставим полученное значение х в выражение для у: 
+//  y = k1 * (b2 - b1) / (k1 - k2) + b1 
+
+//  Получаем: 
+//  x = (b2 - b1) / (k1 - k2)  
+//  y = k1 * (b2 - b1) / (k1 - k2) + b1
+
+
+
+double[] GenerateArray()
+{
+    string[] str;
+    Console.WriteLine("Введите числа через , :");
+    str = Console.ReadLine().Split(",");
+
+    double[] numbers = new double[str.Length];
+    for(int i = 0; i < str.Length; i++)
+    {
+        numbers[i] = Convert.ToDouble(str[i]);
+    }
+
+    return numbers;
+}
+
+
+//=======================  ФУНКЦИЯ ПО РАСЧЕТУ ТОЧКИ ПЕРЕСЕЧЕНИЯ 2-Х ПРЯМЫХ  ======================
+void IntersectionPoint(double[] array)
+{
+    double b1 = array[0];
+    double k1 = array[1];
+    double b2 = array[2];
+    double k2 = array[3];
+
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * (b2 - b1) / (k1 - k2) + b1;
+
+    Console.Write($"Точка пересечения для заданных данных будет: x = {x}, y = {y}.");
+}
+
+
+void Main()
+{
+    double[] array = GenerateArray();
+    IntersectionPoint(array);
+}
+
+Main();
